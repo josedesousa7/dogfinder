cd5bcbd4370562d46bd6ddf1c698f29ec0e322f4
José Marques <jose.marques@xing.com>
2023-01-14 09:53:11 +0000

    Added project to github

---
+++ DogFinder/DogFinder/DogFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogFinderApp.swift
+//  DogFinder
+//
+//  Created by José Marques on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct DogFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
