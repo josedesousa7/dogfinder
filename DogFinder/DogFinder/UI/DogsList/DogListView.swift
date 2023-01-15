d6b10fd140718975038e7eb80a21090661de47fc
José Marques <jose.marques@xing.com>
2023-01-15 15:03:01 +0000

    formatted listView

--- DogFinder/DogFinder/UI/DogsList/DogListView.swift
+++ DogFinder/DogFinder/UI/DogsList/DogListView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct DogListView: View {
 
     @ObservedObject var viewModel: DogListViewModel
+    private var shape = Circle()
 
     init(viewModel: DogListViewModel = DogListViewModel()) {
         self.viewModel = viewModel
@@ -18,19 +19,37 @@ struct DogListView: View {
     var body: some View {
         List {
             ForEach(viewModel.availableDogs, id: \.name) { item in
-                AsyncImage(
-                    url: URL(string: item.imageUrl),
-                    content: { image in
-                        image.resizable()
-                             .aspectRatio(contentMode: .fit)
-                             .frame(maxWidth: 80, maxHeight: 80)
-                    },
-                    placeholder: {
-                        ProgressView()
-                    }
-                )
+                HStack(spacing: 12) {
+                    dogPicture(url: item.imageUrl)
+                    dogName(item.name)
+                    Spacer()
+                }
             }
+            .frame(height: 100)
         }.redacted(reason: viewModel.state.isLoading ? .placeholder: [])
+    }
+
+    @ViewBuilder private func dogPicture(url: String) -> some View {
+        AsyncImage(
+            url: URL(string: url),
+            content: { image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80, height: 80)
+                    .clipShape(shape)
+                    .overlay(shape
+                            .stroke(.gray, lineWidth: 1)
+                    )
+            },
+            placeholder: {
+                Image(systemName: "photo")
+            }
+        )
+    }
+
+    @ViewBuilder private func dogName(_ name: String) -> some View {
+        Text(name)
+            .font(.subheadline)
     }
 }
 
